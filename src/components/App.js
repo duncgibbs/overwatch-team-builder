2e8340c2e6ff87b17fa47b8d81f6499511d4e24e
Duncan Gibbs <duncgibbs@gmail.com>
2017-03-07 09:54:09 -0600

    uses tab width of 4 because we live in a society with rules

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../assets/images/logo_with_title.png';
 import '../assets/stylesheets/App.css';
+import readdirSync from 'fs';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Team Builder</h2>
-        </div>
-        <div className="team-display">
-          
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Team Builder</h2>
+                </div>
+                <div className="team-display">
+                </div>
+                <div className="hero-picker">
+                    <HeroPicker />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
